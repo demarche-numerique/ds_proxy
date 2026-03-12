b35d9bb3ec65f89e5ce83e4f93e232547e583fe7
mfo <mfo@users.noreply.github.com>
2026-03-12 17:19:51 +0100

    test(path_traversal): with percent encoded char, passthru protection

--- tests/traversal_attack.rs
+++ tests/traversal_attack.rs
@@ -10,3 +10,13 @@ fn traversal_attack_is_avoided() {
     println!("curl_download: {:?}", curl_download);
     assert_eq!(curl_download, "404");
 }
+
+#[test]
+#[serial(servers)]
+fn traversal_attack_with_encoded_characters_is_avoided() {
+    let _proxy_node_and_redis = ProxyAndNode::start();
+
+    let curl_download = curl_get_status("localhost:4444/upstream/..%2f..%2fout_of_jail.txt");
+    println!("curl_download: {:?}", curl_download);
+    assert_eq!(curl_download, "404");
+}
